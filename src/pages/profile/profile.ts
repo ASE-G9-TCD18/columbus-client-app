a6fb4e35aab139af2154a11a4a0e3ff150ecb32c
Zen <zhengxu5511@gmail.com>
2018-02-21 15:36:42 +0000

    integrate profile API

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {NavController, LoadingController} from 'ionic-angular';
 import { TripPage } from '../trip/trip';
 import { TripHistoryPage } from '../trip-history/trip-history';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 import { UserData } from '../../providers/user-data';
+import {ConferenceData} from '../../providers/conference-data';
 
 /**
  * Generated class for the ProfilePage page.
@@ -11,22 +13,47 @@ import { UserData } from '../../providers/user-data';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
-})
-export class ProfilePage {
+ @IonicPage()
+ @Component({
+ 	selector: 'page-profile',
+ 	templateUrl: 'profile.html',
+ })
+ export class ProfilePage {
+ 	responseData: any;
+ 	errMessage: any;
+ 	errresponse: any="200";
+ 	name: string;
+ 	constructor(public loader: LoadingController, public navCtrl: NavController, public userData: UserData, public authservice: ConferenceData) {
+ 	}
 
-  constructor(public navCtrl: NavController, public userData: UserData, public navParams: NavParams) {
-  }
 
-  goToTripDetail() { this.navCtrl.push(TripPage); }
+ 	goToTripDetail() { this.navCtrl.push(TripPage); }
 
-  goToTripHistory() { this.navCtrl.push(TripHistoryPage); }
+ 	goToTripHistory() { this.navCtrl.push(TripHistoryPage); }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-  }
+ 	ionViewDidLoad() {
+ 		let loading = this.loader.create({content: "Contacting Server ,please wait..."});
+ 		this.userData.getUsername().then((id)=>
+ 		{
+ 			let user_id = id
+ 			this.userData.getUsertoken().then((value)=>
+ 			{	
+ 				let token = value
+ 				this.authservice
+ 				.getData('user/'+user_id, token)
+ 				.then(
+ 					(result) => {
+ 						this.responseData = result;
+ 						this.name = this.responseData["firstName"];
+ 					},
+ 					(err) => {
+ 						this.errresponse = err
+ 						console.log(err);
+ 						console.log("here is the error" + this.errresponse.errors[0].defaultMessage);
+ 						loading.dismissAll();
+ 					});
+ 			})
+ 		})
+ 	}
 
-}
+ }
