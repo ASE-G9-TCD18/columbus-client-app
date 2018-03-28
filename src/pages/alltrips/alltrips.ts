5d9198a3ba2b3718a5f70faaf2839e6d830ebc55
Ashish Lochan <lochana@tcd.ie>
2018-03-28 14:54:45 +0100

    membership

--- src/pages/alltrips/alltrips.ts
+++ src/pages/alltrips/alltrips.ts
@@ -39,36 +39,67 @@ export class AlltripsPage {
 
   tripdata:any[] = [];
   isLoaded: Boolean = false;
+  admin:any = this.userData.getUsername();
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad AlltripsPage');
     this.isLoaded = true;
-    this.userData.getUsertoken().then((value)=>{
-      this.token = value;
-      console.log("------i am here-----"+value);
-      this.tripdetails.alltriplist(value).then((value: any[]) => {
-        this.tripdata = value;
-        // this.nativeGeocoder.reverseGeocode(52.5072095, 13.1452818)
-        //   .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
-        //   .catch((error: any) => console.log(error));
-        console.log("==========="+this.tripdata[0].tripType);
+    try {
+      this.userData.getUsertoken().then((value) => {
+        this.token = value;
+        console.log("------i am here-----" + value);
+        this.tripdetails.alltriplist(value).then((value: any[]) => {
+          this.tripdata = value;
 
-      })
-    });
+          // this.nativeGeocoder.reverseGeocode(52.5072095, 13.1452818)
+          //   .then((result: NativeGeocoderReverseResult) => console.log(JSON.stringify(result)))
+          //   .catch((error: any) => console.log(error));
+          console.log("===========" + this.tripdata[0].tripType);
+
+        })
+      });
+    }catch(error)
+    {
+      console.log("Error Loading trip data");
+      alert("Error Loading Data. Please try after some time");
+      throw new Error("Error Loading trip data. Contact admin");
+    }
   }
 
   joinTrip(trip){
-    alert("You have now joined this trip !")
-    this.userData.getUsertoken().then((value)=>{
-      this.confData
-        .getData('trip/'+trip.tripId+'/join', value)
-        .then(
-          (result) => {
-            console.log(result);
-            this.navCtrl.push(TripPage, trip);
-          },
-          (err) => {
-            console.log(err);
-          });
-    })
+
+
+      console.log(trip);
+      let i: number;
+      let memberalready: any = false;
+      for(i=0; i<trip.tripUsersLoginIds; i++){
+        if(this.userData.getUsername()==trip.tripUsersLoginIds[i])
+          memberalready = true;
+      }
+     if (trip.admin == this.admin || memberalready) {
+       alert("You have already joined this trip");
+     }
+    else {
+
+      try {
+        this.userData.getUsertoken().then((value) => {
+          this.confData
+            .getData('trip/' + trip.tripId + '/join', value)
+            .then(
+              (result) => {
+                console.log(result);
+                this.navCtrl.push(TripPage, trip);
+              },
+              (err) => {
+                console.log(err);
+              });
+        })
+      } catch (error) {
+        console.log("Error Joining trip");
+        alert("Error Joining Trip. Please try after some time");
+        throw new Error("Error Joining trip. Contact admin");
+      }
+      alert("Your Join request has been sent to Admin! Please Check MyTrips page.");
+    }
   }
 }
