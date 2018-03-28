b4bd7e80281cf09b54fae0b6e4270c541b0445a8
Zen <zhengxu5511@gmail.com>
2018-03-28 14:53:38 +0100

    xu changes

--- src/pages/pending-requests/pending-requests.ts
+++ src/pages/pending-requests/pending-requests.ts
@@ -1,6 +1,7 @@
+import { IonicPage, NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { UserData } from '../../providers/user-data';
+import {ConferenceData} from '../../providers/conference-data';
 /**
  * Generated class for the PendingRequestsPage page.
  *
@@ -15,11 +16,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PendingRequestsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+   constructor(public loader: LoadingController, public navCtrl: NavController, public userData: UserData, public authservice: ConferenceData, public alertCtrl: AlertController) {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PendingRequestsPage');
-  }
+   }
+   responseData: any;
+   errMessage: any;
+   errresponse: any="200";
+   name: string;
+   rate: string;
+
+   ionViewDidLoad() {
+ 	let loading = this.loader.create({content: "Contacting Server ,please wait..."});
+ 	this.userData.getUsername().then((id)=>
+ 	{
+ 		let user_id = id
+ 		this.userData.getUsertoken().then((value)=>
+ 		{	
+ 			let token = value
+ 			this.authservice
+ 			.getData('user/'+user_id, token)
+ 			.then(
+ 				(result) => {
+ 					this.responseData = result;
+ 					console.log(result);
+ 					this.name = this.responseData["firstName"];
+ 					this.rate = this.responseData["userRating"];
+ 				},
+ 				(err) => {
+ 					this.errresponse = err
+ 					console.log(err);
+ 					console.log("here is the error" + this.errresponse.errors[0].defaultMessage);
+ 					loading.dismissAll();
+ 				});
+ 		})
+ 	})
+ }
 
 }
--- src/providers/conference-data.ts
+++ src/providers/conference-data.ts
@@ -8,12 +8,8 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
 
-
-let apiURL = "http://10.6.50.162:8080/"
-
 // let apiURL = "http://52.212.149.132:8080/"
-
-
+let apiURL = "http://10.6.50.162:8080/"
 
 @Injectable()
 export class ConferenceData {
