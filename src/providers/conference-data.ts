deccbdd9d470a6c9c6c49b15ec4e24c50c4d612d
Rahul Agrahari <agraharisrm@gmail.com>
2018-03-29 17:12:09 +0100

    update

--- src/providers/conference-data.ts
+++ src/providers/conference-data.ts
@@ -8,13 +8,9 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/of';
 
-// let apiURL = "http://52.212.149.132:8080/"
-let apiURL = "http://10.6.50.162:8080/"
-
+let apiURL = "http://52.212.149.132:8080/"
 // let apiURL = "http://10.6.50.162:8080/"
 
-
-// let apiURL = "http://52.212.149.132:8080/"
 
 
 @Injectable()
